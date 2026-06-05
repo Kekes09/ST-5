faa9b9b1952e2d574cbe440c82510a9cdeef830b
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2026-06-05 19:16:40 +0300

    reworked

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,7 +2,7 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        double val = Double.parseDouble("2.0");
+        double val = Double.parseDouble("64.0");
         Sqrt sqrt = new Sqrt(val);
         double result = sqrt.calc();
         System.out.println("Sqrt of " + val + " = " + result);
